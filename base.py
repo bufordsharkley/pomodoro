934a792cdc7e58dd981e303cc1bd55c190b1a9e9
Mark Mollineaux <bufordsharkley@gmail.com>
2016-07-14 16:14:41 -0700

    Functionality, mostly

--- base.py
+++ base.py
@@ -2,12 +2,30 @@ import collections
 import datetime
 import enum
 import os
+import select
 import time
 import sys
 import random
 
 
-states = enum.Enum('State', 'sleep live')
+class Ring(Exception):
+  """A pomodoro has ended."""
+
+
+class Ennui(Exception):
+  """It is time for a new pomodoro."""
+
+
+class Dispair(Exception):
+  """You missed your chance for a new pomodoro."""
+
+
+class Hope(Exception):
+  """Even though you have failed, you can still do another pomodoro."""
+
+STATES = enum.Enum('State', 'sleep live notwork nagging gnashing')
+
+NAG_TIME = 120
 
 
 def align(minutes: int, alignment: int) -> int:
@@ -17,122 +35,113 @@ def align(minutes: int, alignment: int) -> int:
 PomodoroState = collections.namedtuple('PomodoroState', ['state', 'seconds'])
 
 
-def advance(state: PomodoroState, seconds: int) -> PomodoroState:
-  if state.state != states.live:
+def advance_state(state: PomodoroState, seconds: int) -> PomodoroState:
+  current_state = state.state
+  if current_state == STATES.sleep:
     return state
   seconds = state.seconds - seconds
   if seconds <= 0:
-    raise Ring
-  return PomodoroState(state=states.live, seconds=seconds)
+    if current_state == STATES.live:
+      raise Ring
+    elif current_state == STATES.notwork:
+      raise Ennui
+    elif current_state == STATES.nagging:
+      raise Dispair
+    elif current_state == STATES.gnashing:
+      raise Hope
+  return PomodoroState(state=current_state, seconds=seconds)
 
 
 def wind_up(seconds: int=0, minutes: int=0) -> PomodoroState:
-  return PomodoroState(state=states.live, seconds=seconds + minutes * 60)
+  return PomodoroState(state=STATES.live, seconds=seconds + minutes * 60)
 
 
 class Pomodoro:
 
   def __init__(self, alignment=0):
     self.alignment = alignment
-    self.state = PomodoroState(state=states.sleep, seconds=0)
+    self.log = []
+    self.state = PomodoroState(state=STATES.sleep, seconds=0)
+    self.final = False
 
-
-class Ring(Exception):
-  pass
-
-
-def demand_wind_up(timeout):
-  import select
-  print ("What's this pomodoro for? "
-         "You have {} seconds to answer!").format(timeout)
-
-  i, o, e = select.select( [sys.stdin], [], [], timeout)
-
-  if (i):
-    return sys.stdin.readline().strip()
-  else:
-    print("You said nothing!")
-
-
-def calculate_work_minutes(seconds: int) -> int:
-  delay = 30 * 60 - seconds
-  return (23 * 60 + delay * 2.5) / 60.
-
-
-def main(args):
-  todos = []
-  with pomodoro_lock():
-    if len(args) > 1:
-      if args[1] == 'start':
-        start()
-      if args[1] == 'end':
-        end(todos)
+  def run_forever(self):
     while True:
       try:
-        now = datetime.datetime.now()
-        if now.minute % 30 < 3:  # in case it runs late for some reason
-          try:
-            todo = demand_wind_up(timeout=2*60)
-            if todo is None:
-              continue
-            now = datetime.datetime.now()
-            minutes_to_work = calculate_work_minutes(now)
-            todos.append(todo)
-            do_work(minutes_to_work * 60)
-            do_not_work((30 - minutes_to_work) * 60)
-          except TimeoutException:
-            pass
+        self.state = advance_state(self.state, 1)
+        self.state = blocking_input_if_needed(self.state,
+                                              self.log,
+                                              self.alignment)
+        # Update visuals:
+        # TODO actual visuals
+        print(self.state)
+        time.sleep(1)
+      except Ring:
+        if self.final:
+          self.final = False
+          self.state = PomodoroState(state=STATES.sleep, seconds=0)
+          #raise StopIteration
         else:
-          if not now.second:
-            print('waiting')
-          time.sleep(1)
+          now = datetime.datetime.now()
+          self.state = PomodoroState(
+              state=STATES.notwork,
+              seconds=seconds_til_next_alignment(now, self.alignment))
+      except (Ennui, Hope):
+        self.state = PomodoroState(state=STATES.nagging, seconds=NAG_TIME)
+      except Dispair:
+        now = datetime.datetime.now()
+        self.state = PomodoroState(
+            state=STATES.gnashing,
+            seconds=seconds_til_next_alignment(now, self.alignment))
       except KeyboardInterrupt:
-        end(todos)
+        # Go into close:
+        print
+        print(self.log)
+        print
+        self.final = True
+        self.state = PomodoroState(state=STATES.live, seconds=12.5 * 60)
 
 
-def start():
-  do_work(12.5 * 60)
-  do_not_work(5 * 60)
+def seconds_til_next_alignment(time_: datetime.datetime, alignment: int) -> int:
+  return 60 * (alignment + 30) - (time_.minute * 60 + time_.second) % (30 * 60)
 
 
-def end(todos):
-  print(todos)
-  do_work(12.5 * 60)
-  raise StopIteration
+def blocking_input_if_needed(state: PomodoroState,
+                             log: list,
+                             alignment: int) -> PomodoroState:
+  if state.state == STATES.sleep:
+    return block_in_sleep(log)
+  elif state.state == STATES.nagging:
+    return block_in_nagging(state.seconds, log, alignment)
+  return state
 
 
-def ring():
-  ring_count = 200
-  for _ in range(ring_count):
-    spaces = ' ' * random.randint(0, 20)
-    print(spaces + 'ring')
-    time.sleep(.5 / float(ring_count))
+def block_in_sleep(log):
+  while log:
+    log.pop()
+  answer = input('Enter something to wake up')
+  return wind_up(minutes=12, seconds=30)
 
 
-def do_work(time_left):
-  time_left = int(time_left)
-  print(time_left)
-  while time_left:
-    if not time_left % 60:
-      print((str(time_left // 60) + '  ') * 100)
-    print('tick')
-    time.sleep(1)
-    time_left -= 1
-  ring()
+def block_in_nagging(timeout: int, log: list, alignment: int) -> PomodoroState:
+  print(("What's this pomodoro for? "
+         "You have {} seconds to answer!").format(timeout))
+
+  answer, _, _ = select.select([sys.stdin], [], [], timeout)
+
+  if (answer):
+    now = datetime.datetime.now()
+    seconds = seconds_til_next_alignment(now, alignment)
+    seconds = calculate_work_seconds(seconds)
+    log.append((now, sys.stdin.readline().strip()))
+    return PomodoroState(state=STATES.live, seconds=seconds)
+  else:
+    raise Dispair
 
 
-def do_not_work(time_left):
-  time_left = int(time_left)
-  while time_left:
-    if not time_left % 60:
-      print(str(time_left // 60) * 100)
-    if time_left <= 30:
-      print(str(time_left) * 100)
-    time.sleep(1)
-    time_left -= 1
+def calculate_work_seconds(total_seconds: int) -> int:
+  return 25 * 60
 
 
-
-class TimeoutException(Exception):
-  pass
-
+def calculate_work_minutes(seconds: int) -> float:
+  delay = 30 * 60 - seconds
+  return (23 * 60 + delay * 2.5) / 60.
--- pomodoro_test.py
+++ pomodoro_test.py
@@ -7,32 +7,48 @@ import base
 class PomodoroStateTests(unittest.TestCase):
 
   def testAdvancingWorks(self):
-    pom = base.PomodoroState(state=base.states.live,
+    for state in (base.STATES.live, base.STATES.notwork, base.STATES.nagging):
+      pom = base.PomodoroState(state=state,
+                               seconds=73)
+      pom = base.advance_state(pom, seconds=3)
+      self.assertEqual(pom.state, state)
+      self.assertEqual(pom.seconds, 70,
+                       'Advance did not work for ' + str(state))
+
+  def testAdvancingNotWorkWorks(self):
+    pom = base.PomodoroState(state=base.STATES.notwork,
                              seconds=73)
-    pom = base.advance(pom, seconds=3)
+    pom = base.advance_state(pom, seconds=3)
     self.assertEqual(pom.seconds, 70)
+    self.assertEqual(pom.state, base.STATES.notwork)
 
   def testTimeRunningDownEmitsRing(self):
-    pom = base.PomodoroState(state=base.states.live,
+    pom = base.PomodoroState(state=base.STATES.live,
                              seconds=60)
     with self.assertRaises(base.Ring):
-      base.advance(pom, seconds=60)
+      base.advance_state(pom, seconds=60)
+
+  def testTimeRunningDownInNotWorkEmitsEnnui(self):
+    pom = base.PomodoroState(state=base.STATES.notwork,
+                             seconds=60)
+    with self.assertRaises(base.Ennui):
+      base.advance_state(pom, seconds=60)
 
   def testTimeRunningDownEmitsRingIfOvershot(self):
-    pom = base.PomodoroState(state=base.states.live,
+    pom = base.PomodoroState(state=base.STATES.live,
                              seconds=60)
     with self.assertRaises(base.Ring):
-      base.advance(pom, seconds=61)
+      base.advance_state(pom, seconds=61)
 
   def testAdvancingDoesntAffectUntickingPomodoro(self):
-    pom = base.PomodoroState(state=base.states.sleep,
+    pom = base.PomodoroState(state=base.STATES.sleep,
                              seconds=0)
-    new_pom = base.advance(pom, seconds=3)
+    new_pom = base.advance_state(pom, seconds=3)
     self.assertEqual(pom, new_pom)
 
   def testWindUpCreatesProperState(self):
     pom = base.wind_up(minutes=23)
-    self.assertEqual(pom.state, base.states.live)
+    self.assertEqual(pom.state, base.STATES.live)
     self.assertEqual(pom.seconds, 23 * 60)
 
   def testWindUpAlsoWorksWithSeconds(self):
@@ -45,6 +61,24 @@ class PomodoroStateTests(unittest.TestCase):
     pom = base.wind_up(seconds=5)
     self.assertEqual(pom.seconds, 5)
     pom = base.wind_up(seconds=5, minutes=2)
+
+
+class PomodoroAlignmentTests(unittest.TestCase):
+
+  def testAlignmentForNormalCase(self):
+    dt = datetime.datetime(1985, 9, 4, 11, 25)
+    seconds = base.seconds_til_next_alignment(dt, alignment=0)
+    self.assertEqual(seconds, 5 * 60)
+
+  def testAlignmentForNormalCasePastHalfHour(self):
+    dt = datetime.datetime(1985, 9, 4, 11, 55)
+    seconds = base.seconds_til_next_alignment(dt, alignment=0)
+    self.assertEqual(seconds, 5 * 60)
+
+  def testAlignmentForImperfectAlignment(self):
+    dt = datetime.datetime(1985, 9, 4, 11, 25)
+    seconds = base.seconds_til_next_alignment(dt, alignment=1)
+    self.assertEqual(seconds, 6 * 60)
 
 
 class PomodoroObjectTests(unittest.TestCase):
@@ -64,7 +98,11 @@ class PomodoroObjectTests(unittest.TestCase):
 
   def testDefaultStateIsSleep(self):
     pom = base.Pomodoro()
-    self.assertEqual(pom.state.state, base.states.sleep)
+    self.assertEqual(pom.state.state, base.STATES.sleep)
+
+  def testDefaultLogIsEmpty(self):
+    pom = base.Pomodoro()
+    self.assertEqual(pom.log, [])
 
 
 class PomodoroTests(unittest.TestCase):
